2ecb03419372d491f8f378dd9c637e7f6eaf2c1f
Hemant Kumar <kumar.hemant1797@gmail.com>
2026-04-01 02:53:53 +0530

    Phase 1: Django setup with Task model and admin functionality

---
+++ todo/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    PRIORITY_CHOICES=[
+        ('Low', 'Low'),
+        ('Medium', 'Medium'),
+        ('High', 'High'),
+    ]
+
+    title=models.CharField(max_length=200)
+    completed=models.BooleanField(default=False)
+    created_at= models.DateTimeField(auto_now_add=True)
+    due_date=models.DateField(null=True, blank=True)
+    priority= models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='Medium')
+
+    def __str__(self):
+        return self.title
